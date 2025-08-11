fixing bug in grad normalization

src/quantem/diffractive_imaging/probe_models.py
@@ -582,7 +582,7 @@ class ProbePixelated(ProbeConstraints):
         return "ProbePixelized"
 
     def backward(self, propagated_gradient, obj_patches):
-        obj_normalization = torch.sum(torch.abs(obj_patches) ** 2, dim=(0, 1)).max()
+        obj_normalization = torch.sum(torch.abs(obj_patches) ** 2, dim=(-2, -1)).max()
         ortho_norm: float = np.prod(self.roi_shape) ** 0.5  # from ortho fft2 # type:ignore
         probe_grad = torch.sum(propagated_gradient, dim=1) / obj_normalization / ortho_norm
         self._probe.grad = -1 * probe_grad.clone().detach()

src/quantem/diffractive_imaging/ptychography_base.py
@@ -897,14 +897,15 @@ class PtychographyBase(RNGMixin, AutoSerialize):
             else:
                 targets = self.dset.centered_amplitudes[batch_indices]
             preds = torch.sqrt(pred_intensities + 1e-9)  # add eps to avoid diverging gradients
-            norm = self.dset.mean_diffraction_intensity  # sgd is more stable with amplitude norm
+            norm = self.dset.mean_diffraction_intensity
         else:
             if use_unshifted:
                 targets = self.dset.intensities[batch_indices]
             else:
                 targets = self.dset.centered_intensities[batch_indices]
             preds = pred_intensities
-            norm = self.dset.mean_diffraction_intensity**1.5  # otherwise sgd diverges??
+            norm = self.dset.mean_diffraction_intensity  # ??
+            # norm = self.dset.mean_diffraction_intensity**1.5  # otherwise sgd diverges??
 
         diff = preds - targets
         if loss_type == "l1":