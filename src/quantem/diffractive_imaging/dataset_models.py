6ff7caf76e4b3bc08bcccd7c1ce6667e6a49a633
arthurmccray <amccray@stanford.edu>
2025-08-18 11:48:10 -0700

    adding probe model class methods, fixing ad grads upsampling factor

--- src/quantem/diffractive_imaging/dataset_models.py
+++ src/quantem/diffractive_imaging/dataset_models.py
@@ -520,6 +520,10 @@ class PtychographyDatasetBase(AutoSerialize, OptimizerMixin, torch.nn.Module):
         rotshape += 2 * np.array(obj_padding_px)
         return rotshape.astype("int")
 
+    @property
+    def upsample_factor(self) -> float:
+        return (self._obj_shape_crop_2d / self.shape[:2]).mean()
+
     # endregion --- implicit properties (no setters) ---
 
     # region --- abstract class methods ---
